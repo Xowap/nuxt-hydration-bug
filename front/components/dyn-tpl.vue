b775ee2d538912c4505b600201f7a46fff3e0ec3
Rémy Sanchez <remy.sanchez@hyperthese.net>
2023-09-07 15:43:37 +0200

    Strip down and still get the bug

---
+++ front/components/dyn-tpl.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    props: {
+        html: {
+            type: String,
+            required: true,
+        },
+    },
+
+    render() {
+        return h({
+            template: this.html,
+        });
+    },
+};
+</script>
---
+++ front/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <DynTpl :html="html as string" />
+</template>
+
+<script setup lang="ts">
+import DynTpl from "~/components/dyn-tpl.vue";
+
+const html = ref<string>(`
+<div>
+    <div style="background-color: aquamarine;">
+        <p>AAAAAAAAA</p>
+    </div>
+    <div style="background-color: yellow;">
+        <p>BBBBBBBBBBB</p>
+    </div>
+    <!-- Comment -->
+    <div style="background-color: orange;">
+        <div>CCCCCCCCCC</div>
+    </div>
+    <div style="background-color: aquamarine;">
+        <p>AAAAAAAAA</p>
+    </div>
+    <div style="background-color: yellow;">
+        <p>BBBBBBBBBBB</p>
+    </div>
+    <!-- Comment 2 -->
+    <div style="background-color: orange;">
+        <div>CCCCCCCCCC</div>
+    </div>
+</div>
+`);
+</script>
